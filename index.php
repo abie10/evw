723b29846359e060cc07161ac7af9a6aa60c6573
Abelicio Padilla <abeliciopadilla@mac-le-00723.corp.lucid.lcl>
2022-12-14 09:58:30 -0700

    adding table

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>HTML Table Example</title>
+</head>
+<body>
+  <table>
+    <tr>
+      <td colspan="2">Column 1</td>
+      <td>Column 2</td>
+      <td colspan="2">Column 3</td>
+    </tr>
+  </table>
+</body>
+</html>
+
